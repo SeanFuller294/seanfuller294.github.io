520f59a2634e9a9f49855e47616c2bd008609eda
SeanFuller294 <s.fuller294@hotmail.com>
2019-08-12 14:06:45 -0600

    Added js button links around page

---
+++ app.js
@@ -0,0 +1,23 @@
+let myIntroRow = document.getElementById("my-intro-row");
+function scrollToIntro() {
+  myIntroRow.scrollIntoView(true);
+}
+let myAboutRow = document.getElementById("my-about-row");
+function scrollToAbout() {
+  myAboutRow.scrollIntoView(true);
+}
+let mySkillsRow = document.getElementById("my-skills-row");
+function scrollToSkills() {
+  mySkillsRow.scrollIntoView(true);
+}
+let myProjectsRow = document.getElementById("my-project-row");
+function scrollToProjects() {
+  myProjectsRow.scrollIntoView(true);
+}
+let myContactRow = document.getElementById("my-contact-row");
+function scrollToContact() {
+  myContactRow.scrollIntoView(true);
+}
+
+
+
